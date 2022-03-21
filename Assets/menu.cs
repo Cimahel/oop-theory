c9ae4c2d9259d0eff784f304fd344bc93169b96f
Cimahel <michaeleahcim@hotmail.com>
2022-03-20 22:37:30 -0500

    Update menu.cs

--- Assets/menu.cs
+++ Assets/menu.cs
@@ -8,7 +8,7 @@ public class menu : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        DontDestroyOnLoad(this);
+        DontDestroyOnLoad(gameObject);
     }
 
     public void StartScene()
